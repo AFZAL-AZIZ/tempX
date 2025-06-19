3736f67c8a87d76b457e425a855aa812cc37032d
AFZAL AZIZ <afzalaziz01@gmail.com>
2025-06-19 12:48:23 +0530

    Coverted to async/await

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,43 +1,41 @@
 /** @type {import('mongoose').Model<any>} */
 const product = require("./../models/productModel");
 
-exports.getAllProducts = (req, res) => {
-  product
-    .find()
-    .then((products) => {
-      res.status(200).json({
-        status: "success",
-        results: products.length,
-        data: {
-          products: products
-        }
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        status: "failed",
-      });
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await product.find();
+    res.status(200).json({
+      status: "success",
+      results: products.length,
+      data: {
+        products: products,
+      },
     });
+  } catch (err) {
+    res.status(500).json({
+      status: "failed",
+    });
+  }
 };
 
-exports.createProduct = (req, res) => {
-  product
-    .create(req.body)
-    .then((newProduct) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          product: newProduct
-        }
-      });
-    })
-    .catch((err) => {
-      res.status(400).json({
-        status: "failed",
-        message: err
-      });
+
+exports.createProduct = async (req, res) => {
+  try {
+    const newProduct = await product.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        product: newProduct,
+      },
     });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: err.message || err,
+    });
+  }
 };
+
 
 exports.getProduct = (req, res) => {
   res.json({
